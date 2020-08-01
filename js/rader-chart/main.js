9fd3f995f3dccaf20d86f7e28290c8f6a254d3c7
daich <fst-qst.w-it-h@docomo.ne.jp>
2020-08-01 11:46:23 +0100

    20200801 try to create base content

---
+++ js/rader-chart/main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    chartbuild();
+};
+
+function chartbuild() {
+
+    var canvas = document.getElementById('rader-chart');
+    if (canvas.getContext) {
+
+        var context = canvas.getContext('2d');
+
+    }
+}
