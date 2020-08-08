985aa96fdd06d32d70a510f1e139e06a9f06a713
daich <fst-qst.w-it-h@docomo.ne.jp>
2020-08-08 09:00:44 +0100

    20200808 try to create params chart

--- js/rader-chart/main.js
+++ js/rader-chart/main.js
@@ -10,12 +10,21 @@ function chartbuild() {
     //チャートの基本変数定義
     var center_x = 100;          //中心x座標
     var center_y = 100;          //中心y座標
-    var vertex_num = 6;              //頂点の数
-    var param_values = new Array(vertex_num);
+    var paramValuesEle = document.getElementById("param-values");
+    var param_values = new Array(
+        paramValuesEle.dataset["chrPwrReal"],
+        paramValuesEle.dataset["chrPwrIdeal"],
+        paramValuesEle.dataset["cntrlDifficult"],
+        paramValuesEle.dataset["advntageHighRankChr"],
+        paramValuesEle.dataset["hame"],
+        paramValuesEle.dataset["stability"]
+    );
+    var vertex_num = param_values.length;              //頂点の数
     var chart_radius = 100;              //大きさ（外接円の半径）
     var tilt = 180;               //傾き（０だと１つ目の頂点が真下にくる）
     var scale_num = 5;               //目盛の数
     var line_color = "#a9a9a9";  //チャートのライン色
+    var params_line_color = "#ffc9da";  //パラメータのチャートのライン色
     var line_width = 2;          //チャートのラインの太さ
     var line_width_out = 4;      //チャートのラインの太さ（一番外側）
     var line_width_center = 1.5;   //チャートの中心線の太さ
@@ -34,7 +43,6 @@ function chartbuild() {
          */
         function reset() {
             makechartframe();
-            setParamValues();
             makecChartFromParams();
             makecenterline();
         }
@@ -45,7 +53,6 @@ function chartbuild() {
         function makechartframe() {
             var x;
             var y;
-            // var one_scale_length;
             var angle;
             var rad;
 
@@ -99,60 +106,99 @@ function chartbuild() {
             }
         }
 
-        // 動的パラメータ値を設定する
-        function setParamValues() {
-                // チャートの頂点の数だけ繰り返す
-                for (var l = 0; l < vertex_num; l++) {
-                    // TODO: とりあえず静的に2を設定している
-                    param_values[l] = 2;
-            }
-        }
-
         //パラメータ値に沿ったチャートを描画する
         function makecChartFromParams() {
-
-        }
-
-        //チャートの中心線を描画する
-        function makecenterline() {
-
             var x;
             var y;
             var angle;
             var rad;
-            // 線の色を設定する
-            context.strokeStyle = line_color;
-            // 線の幅を設定する
-            context.lineWidth = line_width_center;
+            // 初回パラメータの描画X座標
+            var start_x;
+            // 初回パラメータの描画Y座標
+            var start_y;
 
-            // 多角形の頂点の数だけ繰り返す
-            for (var m = 1; m <= vertex_num; m++) {
-                // 引く中心線の角度を求める
-                angle = tilt + 360 / vertex_num * m;
-                // ラジアン（弧度）を求める
+            // パラメータの数だけ繰り返す
+            for (var i = 0; i < param_values.length; i++) {
+                // 線の色を設定する
+                context.strokeStyle = params_line_color;
+
+                // 描画する位置をリセットする
+                context.beginPath();
+
+                // パラメータ値の分だけ目盛の位置を伸ばす
+                nth_scale_length = chart_radius / scale_num * param_values[i];
+
+                // 描画開始角度を求める
+                angle = tilt + 360 / vertex_num * i;
+                // ラジアン(弧度)を求める ※Math.PI = π
                 rad = angle * Math.PI / 180;
 
-                // 求めた弧度の円周上のX座標を求める
-                x = getXposiOnCircle(center_x, rad, chart_radius);
-                // 求めた弧度の円周上のY座標を求める
-                y = getYposiOnCircle(center_y, rad, chart_radius);
+                // 1目盛目のX座標を求める
+                x = getXposiOnCircle(center_x, rad, nth_scale_length);
+                // 1目盛目のY座標を求める
+                y = getYposiOnCircle(center_y, rad, nth_scale_length);
 
-                context.beginPath();
-                // 円の中心へ移動する
-                context.moveTo(center_x, center_y);
-                // 求めた円周上の座標まで線を引く
-                context.lineTo(x, y);
-                context.stroke();
+                // 初回の繰り返しのみ描画開始位置を初期化する
+                if (i == 0) {
+                    // 描画開始位置へ移動する
+                    context.moveTo(x, y);
+                    // 描画開始位置を保持する
+                    start_x = x;
+                    start_y = y;
+                } else {
+                    // 指定位置まで線を引く
+                    context.lineTo(x, y);
+                }
+                console.log("i : " + i + " x : " + x + " y : " + y + " rad : " + rad);
             }
+            // 描画開始位置まで線を引く
+            context.lineTo(start_x, start_y);
+            // 今まで指定した線を描画する
+            // ※stroke = 指定した輪郭を描画, fill = 指定した内部エリアを塗りつぶす
+            context.stroke();
         }
     }
 
-            /**
-         * 指定したX座標を中心として、指定した半径の円を引き、指定した角度の円上のX座標を返す
-         * @param {*} centerX 円の中央X座標
-         * @param {*} radius 円からの角度
-         * @param {*} nthScaleLength 円の半径 
-         */
+    //チャートの中心線を描画する
+    function makecenterline() {
+
+        var x;
+        var y;
+        var angle;
+        var rad;
+        // 線の色を設定する
+        context.strokeStyle = line_color;
+        // 線の幅を設定する
+        context.lineWidth = line_width_center;
+
+        // 多角形の頂点の数だけ繰り返す
+        for (var m = 1; m <= vertex_num; m++) {
+            // 引く中心線の角度を求める
+            angle = tilt + 360 / vertex_num * m;
+            // ラジアン（弧度）を求める
+            rad = angle * Math.PI / 180;
+
+            // 求めた弧度の円周上のX座標を求める
+            x = getXposiOnCircle(center_x, rad, chart_radius);
+            // 求めた弧度の円周上のY座標を求める
+            y = getYposiOnCircle(center_y, rad, chart_radius);
+
+            context.beginPath();
+            // 円の中心へ移動する
+            context.moveTo(center_x, center_y);
+            // 求めた円周上の座標まで線を引く
+            context.lineTo(x, y);
+            context.stroke();
+        }
+    }
+
+
+    /**
+    * 指定したX座標を中心として、指定した半径の円を引き、指定した角度の円上のX座標を返す
+    * @param {*} centerX 円の中央X座標
+    * @param {*} radius 円からの角度
+    * @param {*} nthScaleLength 円の半径 
+    */
     function getXposiOnCircle(centerX, radius, nthScaleLength) {
         return centerX + Math.sin(radius) * nthScaleLength;
     }
